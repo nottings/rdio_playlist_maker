f053a1040686dbf6882f48a0c067b5d14d24f089
nottings <scott.nottingham@gmail.com>
2014-03-29 11:12:27 -0500

    add option to allow user to specify the number of tracks per artist to retrieve

--- rdio_pl_maker.py
+++ rdio_pl_maker.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="Creates an Rdio playlist based on 
 parser.add_argument('-f', dest='input_file', action='store', default=None, help="Input file containing names of artists")
 parser.add_argument('-n', dest='pl_name', action='store', default=None, help="Name for playlist")
 parser.add_argument('-d', dest='pl_desc', action='store', default=None, help="Description for playlist")
+parser.add_argument('-c', dest='track_count', action='store', default=3, help="How many tracks per artists to add to playlist. Defaults to 3")
 args = parser.parse_args()
 
 if not args.input_file or not args.pl_name or not args.pl_desc:
@@ -30,7 +31,7 @@ for a in artists:
     try:
         url = '/artist/' + a.strip().title().replace(' ', '_').replace('&', '') + '/'
         artist_key = r.getObjectFromUrl(url=url)['key']
-        tracks = r.getTracksForArtist(artist=artist_key, count=3)
+        tracks = r.getTracksForArtist(artist=artist_key, count=args.track_count)
         # make sure key is for a track and for the right artist
         track_keys = [t['key'] for t in tracks if t['artistUrl'].lower() == url.lower() and t['key'].startswith('t')]
         for t in track_keys:
