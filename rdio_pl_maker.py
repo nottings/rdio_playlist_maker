20c42beb325c47f7c45df1608ed5954684f3f5de
nottings <scott.nottingham@gmail.com>
2014-03-29 11:15:47 -0500

    PEP

--- rdio_pl_maker.py
+++ rdio_pl_maker.py
@@ -6,15 +6,33 @@ from rdioapi import Rdio
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
 
-parser = argparse.ArgumentParser(description="Creates an Rdio playlist based on artists in <input_file>")
-parser.add_argument('-f', dest='input_file', action='store', default=None, help="Input file containing names of artists")
-parser.add_argument('-n', dest='pl_name', action='store', default=None, help="Name for playlist")
-parser.add_argument('-d', dest='pl_desc', action='store', default=None, help="Description for playlist")
-parser.add_argument('-c', dest='track_count', action='store', default=3, help="How many tracks per artists to add to playlist. Defaults to 3")
+parser = argparse.ArgumentParser(
+        description="Creates an Rdio playlist based on artists in <input_file>")
+parser.add_argument('-f',
+        dest='input_file',
+        action='store',
+        default=None,
+        help="Input file containing names of artists")
+parser.add_argument('-n',
+        dest='pl_name',
+        action='store',
+        default=None,
+        help="Name for playlist")
+parser.add_argument('-d',
+        dest='pl_desc',
+        action='store',
+        default=None,
+        help="Description for playlist")
+parser.add_argument('-c',
+        dest='track_count',
+        action='store',
+        default=3,
+        help="How many tracks per artists to add to playlist. Defaults to 3")
 args = parser.parse_args()
 
 if not args.input_file or not args.pl_name or not args.pl_desc:
-    raise InputError('You must specify an input file containing a list of artists, a name for your playlist, and a description for the playlist')
+    raise InputError('You must specify an input file containing a list of artists, \
+            a name for your playlist, and a description for the playlist')
 
 # Oauth authentication needed for creating the new playlist
 state = {}
@@ -33,7 +51,9 @@ for a in artists:
         artist_key = r.getObjectFromUrl(url=url)['key']
         tracks = r.getTracksForArtist(artist=artist_key, count=args.track_count)
         # make sure key is for a track and for the right artist
-        track_keys = [t['key'] for t in tracks if t['artistUrl'].lower() == url.lower() and t['key'].startswith('t')]
+        track_keys = [t['key'] for t in tracks \
+                if t['artistUrl'].lower() == url.lower() \
+                and t['key'].startswith('t')]
         for t in track_keys:
             pl_tracks.append(t)
     except:
@@ -42,7 +62,8 @@ for a in artists:
 
 # create the playlist
 try:
-    r.call("createPlaylist", name=args.pl_name, description=args.pl_desc, tracks=','.join(pl_tracks))
+    r.call("createPlaylist", name=args.pl_name, description=args.pl_desc, \
+            tracks=','.join(pl_tracks))
 except:
     print 'error creating playlist'
 
